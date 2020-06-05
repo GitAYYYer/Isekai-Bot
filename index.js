adf305493e240ad1764da7b18f495eb97be8e95b
Duc <duc.h@hotmail.com>
2020-06-06 03:15:48 +1000

    do up and display for incrementing and showing level

--- index.js
+++ index.js
@@ -35,11 +35,24 @@ bot.on('message', message => {
             // Not sure benefit of exists vs existsSync, and with appendFile vs appendFileSync
             if (!fs.existsSync(saveDataPath)) {
                 fs.appendFileSync('./saveData.json', '{"player": {"level": 0, "name": "name"}}', function (err) {
-                    if (err) throw err
-                    console.log("Save data created!")
+                    if (err) throw err;
+                    console.log('saved!');
                 });
             }
             break;
+        case 'up':
+            console.log('up command')
+            let currentSaveData = JSON.parse(fs.readFileSync(saveDataPath, 'utf-8'));
+            currentSaveData['player']['level'] = (parseInt(currentSaveData['player']['level']) + 1);
+            JSON.parse(fs.readFileSync(saveDataPath, 'utf-8')).level = parseInt(JSON.parse(fs.readFileSync(saveDataPath, 'utf-8')).level) + 1;
+            fs.writeFileSync(saveDataPath, JSON.stringify(currentSaveData, null, 2), function writeJSON(err) {
+                if (err) throw err;
+            });
+            break;
+        case 'display':
+            console.log('displaying level');
+            let saveDataFile = JSON.parse(fs.readFileSync(saveDataPath, 'utf-8'));
+            message.channel.send('Current level is: ' + saveDataFile['player']['level']);
         case 'ping':
             message.channel.send('don\'t ping me you fuck')
             break;
