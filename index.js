62eeb6b84e146dd4dc420b0422cdcf41f7c4557e
Duc <duc.h@hotmail.com>
2020-06-06 02:33:11 +1000

    basic save data creation if it doesnt exist already

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+// discord imports
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const configFile = require('./config.json');
 
+// regular imports
+const fs = require('fs');
+
+// config files
 const token = configFile.token;
 const prefix = configFile.prefix;
 
+// save related
+const saveDataPath = './saveData.json';
+
 bot.on('ready', () => {
     console.log('Bot is online.')
+
+    if (fs.existsSync(saveDataPath)) {
+        console.log('SaveData exists.');
+    } else {
+        console.log('SaveData does not exist.')
+    }
 })
 
 bot.on('message', message => {
@@ -17,6 +31,15 @@ bot.on('message', message => {
     let args = message.content.slice(prefix.length).split(' ');
 
     switch (args[0].toLowerCase()) {
+        case 'start':
+            // Not sure benefit of exists vs existsSync, and with appendFile vs appendFileSync
+            if (!fs.existsSync(saveDataPath)) {
+                fs.appendFileSync('./saveData.json', '{"player": {"level": 0, "name": "name"}}', function (err) {
+                    if (err) throw err
+                    console.log("Save data created!")
+                });
+            }
+            break;
         case 'ping':
             message.channel.send('don\'t ping me you fuck')
             break;
