eb141f4dfe441dae0a447a2a3104f17f5b07bd8d
Duc <duc.h@hotmail.com>
2020-06-06 15:46:41 +1000

    getSaveData() returns json object, check for create new save file and make new save data for users

--- index.js
+++ index.js
@@ -30,15 +30,15 @@ bot.on("message", (message) => {
 
     switch (args[0].toLowerCase()) {
         case "create":
-            start();
+            start(message);
             break;
 
         case "up":
-            up();
+            up(message);
             break;
 
         case "display":
-            display();
+            display(message);
             break;
 
         case "ping":
@@ -64,27 +64,65 @@ bot.on("message", (message) => {
     }
 });
 
-function start() {
-    // Not sure benefit of exists vs existsSync, and with appendFile vs appendFileSync
-    if (!fs.existsSync(saveDataPath)) {
-        fs.appendFileSync(
-            "./saveData.json",
-            '{"player": {"level": 0, "name": "name"}}',
-            function (err) {
-                if (err) throw err;
-                console.log("saved!");
-            }
-        );
-    }
+/*
+Helper function to return the save data as JSON map.
+No need for arguments, since it's always using the same path.
+*/
+function getSaveData() {
+    let saveData = JSON.parse(fs.readFileSync(saveDataPath, 'utf-8'));
+    return saveData;
 }
 
-function up() {
+/*
+Creates new save data for a user with their id as the key.
+If the saveData.json already exists, return immediately (change in the future to not bother with a file check maybe).
+If id already exists in the file, return immediately.
+*/
+function start(message) {
+    var authorId = message.author.id;
+    // Create the first user in a new file.
+    if (!fs.existsSync(saveDataPath)) {
+        var baseFile = {};
+        baseFile[authorId] = {level: 0, name: 'name'};
+        fs.appendFileSync(
+            saveDataPath,
+            JSON.stringify(baseFile, null, 2),
+            function (err) {
+                if (err) throw err;
+            }
+        );
+        message.channel.send("Save file created. First save is <@" + authorId + ">'s.")
+        return;
+    }
+
+    // If the id already exists in the save file, don't bother with creating a new user.
+    if (getSaveData().hasOwnProperty(authorId)) {
+        message.channel.send("Sorry <@" +authorId + ">, you've already been isekai'd. " 
+        + "Try prestiging to isekai yourself again!");
+        return;
+    }
+
+    // Passed all checks, append a new user id key to the current file.
+    let currentSaveData = getSaveData();
+    currentSaveData[authorId] = {level: 0, name: 'New Name'};
+    fs.writeFileSync(
+        saveDataPath,
+        JSON.stringify(currentSaveData, null, 2),
+        function writeJSON(err) {
+            if (err) throw err;
+        }
+    )
+    message.channel.send("Created a new save for <@" + authorId + ">!");
+}
+
+/*
+Debug method to just level up current player. Don't bother making it clean I think.
+*/
+function up(message) {
     console.log("up command");
-    let currentSaveData = JSON.parse(fs.readFileSync(saveDataPath, "utf-8"));
-    currentSaveData["player"]["level"] =
-        parseInt(currentSaveData["player"]["level"]) + 1;
-    JSON.parse(fs.readFileSync(saveDataPath, "utf-8")).level =
-        parseInt(JSON.parse(fs.readFileSync(saveDataPath, "utf-8")).level) + 1;
+    var authorId = message.author.id;
+    let currentSaveData = getSaveData();
+    currentSaveData[authorId]["level"] = parseInt(currentSaveData[authorId]["level"]) + 1;
     fs.writeFileSync(
         saveDataPath,
         JSON.stringify(currentSaveData, null, 2),
@@ -92,12 +130,15 @@ function up() {
             if (err) throw err;
         }
     );
+    message.channel.send("<@" + authorId + "> leveled up to level " + currentSaveData[authorId]["level"] + "!");
 }
 
-function display() {
+/*
+Debug method to display current level.
+*/
+function display(message) {
     console.log("displaying level");
-    let saveDataFile = JSON.parse(fs.readFileSync(saveDataPath, "utf-8"));
-    message.channel.send("Current level is: " + saveDataFile["player"]["level"]);
+    message.channel.send("Current level is: " + getSaveData()[message.author.id]["level"]);
 }
 
 bot.login(token);
