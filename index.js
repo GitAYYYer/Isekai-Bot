996835d711f6e6365ef087b32dc00a26ebe6e432
User <wesley.rmit@gmail.com>
2020-06-07 20:51:59 +1000

    Moved my (wes) code to the bottom

--- index.js
+++ index.js
@@ -394,139 +394,6 @@ function partySwitch(message, args) {
   }
 }
 
-function balance(message) {
-  let players = getJsonData(saveDataPath);
-  let player;
-  let playerName = mentionUser(message.author.id);
-
-  for (var id in players) {
-    if (id == message.author.id) {
-      player = players[id];
-    }
-  }
-
-  message.channel.send(`${playerName}'s Wallet: $${player.money}`);
-}
-
-function inventory(message) {
-  let players = getJsonData(saveDataPath);
-  let player;
-  let playerName = mentionUser(message.author.id);
-  let inventoryList = "";
-
-  for (var id in players) {
-    if (id == message.author.id) {
-      player = players[id];
-    }
-  }
-  message.channel.send(`${playerName}'s Inventory:`);
-
-  //Log out the number of the items with the same name instead of just having copies (future ticket)
-  for (let item of player.inventory) {
-    inventoryList += `${item.name} x${item.quantity}\n`;
-  }
-
-  if (inventoryList == "") {
-    message.channel.send(`Sorry you don't have anything.`);
-  } else {
-    message.channel.send(`${inventoryList}`);
-  }
-}
-
-//ShopSwitch is called when arg[0] is "shop"
-//Upon "shop" command, shop is opened and list of items are displayed
-function shopSwitch(message, args) {
-  switch (args[1]) {
-    case "list":
-      shopList(message);
-      break;
-    case "buy":
-      shopBuy(message, args[2]);
-      break;
-  }
-}
-
-function shopList(message) {
-  let shop = getJsonData(shopPath);
-  var shopItems = "Shop Items:\n";
-  for (let i = 0; i < shop.items.length; i++) {
-    shopItems += `${shop.items[i].name}  $${shop.items[i].cost}\n`;
-  }
-  message.channel.send(shopItems);
-}
-
-//Shop buy purchases the item from the shop and adds it to your inventoy.
-//Update player's money & inventory
-function shopBuy(message, itemName) {
-  let players = getJsonData(saveDataPath);
-  let player;
-  let playerName = mentionUser(message.author.id);
-  let shop = getJsonData(shopPath);
-  let item;
-
-  //Get player by message.author.id (Perhaps make util methods later for this)
-  for (var id in players) {
-    if (id == message.author.id) {
-      player = players[id];
-    }
-  }
-
-  //Get item by name (Perhaps make util method later for this)
-  for (let itemObj of shop.items) {
-    if (itemObj.name.toLowerCase() == itemName.toLowerCase()) {
-      item = itemObj;
-    }
-  }
-
-  if (item == null) {
-    message.channel.send(
-      `Sorry we don't have any more ${itemName}'s in stock right now`
-    );
-    //Multiple points of exit OMG Tim-Trigger alert
-    return;
-  }
-
-  //Check if player has enough money to buy given item
-  if (player.money - item.cost >= 0) {
-    //Update player balance and add to their inventory
-    message.channel.send(`${playerName}'s Wallet: $${player.money}`);
-    player.money -= item.cost;
-
-    message.channel.send(
-      `${playerName} bought a ${item.name} for $${item.cost}!`
-    );
-
-    //Check if player already has item, if they do increase the count
-
-    let playerItem;
-    let hasItemAlready = false;
-    for (let playerItemTemp of player.inventory) {
-      //Player already has at least one copy of this item
-      if (playerItemTemp.name == item.name) {
-        hasItemAlready = true;
-        playerItem = playerItemTemp;
-      }
-    }
-
-    if (hasItemAlready) {
-      playerItem.quantity += 1;
-    } else {
-      let newItem = { name: item.name, cost: item.cost, quantity: 1 };
-      player.inventory.push(newItem);
-    }
-
-    //Update Balance
-    message.channel.send(`${playerName}'s Wallet: $${player.money}`);
-
-    writeJson(saveDataPath, players);
-  } else {
-    message.channel.send(`${playerName}'s Wallet: $${player.money}`);
-    message.channel.send(
-      `POVO alert! y'all ain't getting y'all dirty ass hands on this ${item.name}!`
-    );
-  }
-}
-
 function partyCreate(message, args) {
   let playerSave = getJsonData(saveDataPath);
   let playerSavePartyId = playerSave[message.author.id]["partyId"];
@@ -711,6 +578,139 @@ function writeJson(path, jsonString) {
 
 function isNull(value) {
   return value == null || value == "" || value == 0;
+}
+
+function balance(message) {
+  let players = getJsonData(saveDataPath);
+  let player;
+  let playerName = mentionUser(message.author.id);
+
+  for (var id in players) {
+    if (id == message.author.id) {
+      player = players[id];
+    }
+  }
+
+  message.channel.send(`${playerName}'s Wallet: $${player.money}`);
+}
+
+function inventory(message) {
+  let players = getJsonData(saveDataPath);
+  let player;
+  let playerName = mentionUser(message.author.id);
+  let inventoryList = "";
+
+  for (var id in players) {
+    if (id == message.author.id) {
+      player = players[id];
+    }
+  }
+  message.channel.send(`${playerName}'s Inventory:`);
+
+  //Log out the number of the items with the same name instead of just having copies (future ticket)
+  for (let item of player.inventory) {
+    inventoryList += `${item.name} x${item.quantity}\n`;
+  }
+
+  if (inventoryList == "") {
+    message.channel.send(`Sorry you don't have anything.`);
+  } else {
+    message.channel.send(`${inventoryList}`);
+  }
+}
+
+//ShopSwitch is called when arg[0] is "shop"
+//Upon "shop" command, shop is opened and list of items are displayed
+function shopSwitch(message, args) {
+  switch (args[1]) {
+    case "list":
+      shopList(message);
+      break;
+    case "buy":
+      shopBuy(message, args[2]);
+      break;
+  }
+}
+
+function shopList(message) {
+  let shop = getJsonData(shopPath);
+  var shopItems = "Shop Items:\n";
+  for (let i = 0; i < shop.items.length; i++) {
+    shopItems += `${shop.items[i].name}  $${shop.items[i].cost}\n`;
+  }
+  message.channel.send(shopItems);
+}
+
+//Shop buy purchases the item from the shop and adds it to your inventoy.
+//Update player's money & inventory
+function shopBuy(message, itemName) {
+  let players = getJsonData(saveDataPath);
+  let player;
+  let playerName = mentionUser(message.author.id);
+  let shop = getJsonData(shopPath);
+  let item;
+
+  //Get player by message.author.id (Perhaps make util methods later for this)
+  for (var id in players) {
+    if (id == message.author.id) {
+      player = players[id];
+    }
+  }
+
+  //Get item by name (Perhaps make util method later for this)
+  for (let itemObj of shop.items) {
+    if (itemObj.name.toLowerCase() == itemName.toLowerCase()) {
+      item = itemObj;
+    }
+  }
+
+  if (item == null) {
+    message.channel.send(
+      `Sorry we don't have any more ${itemName}'s in stock right now`
+    );
+    //Multiple points of exit OMG Tim-Trigger alert
+    return;
+  }
+
+  //Check if player has enough money to buy given item
+  if (player.money - item.cost >= 0) {
+    //Update player balance and add to their inventory
+    message.channel.send(`${playerName}'s Wallet: $${player.money}`);
+    player.money -= item.cost;
+
+    message.channel.send(
+      `${playerName} bought a ${item.name} for $${item.cost}!`
+    );
+
+    //Check if player already has item, if they do increase the count
+
+    let playerItem;
+    let hasItemAlready = false;
+    for (let playerItemTemp of player.inventory) {
+      //Player already has at least one copy of this item
+      if (playerItemTemp.name == item.name) {
+        hasItemAlready = true;
+        playerItem = playerItemTemp;
+      }
+    }
+
+    if (hasItemAlready) {
+      playerItem.quantity += 1;
+    } else {
+      let newItem = { name: item.name, cost: item.cost, quantity: 1 };
+      player.inventory.push(newItem);
+    }
+
+    //Update Balance
+    message.channel.send(`${playerName}'s Wallet: $${player.money}`);
+
+    writeJson(saveDataPath, players);
+  } else {
+    message.channel.send(`${playerName}'s Wallet: $${player.money}`);
+    message.channel.send(
+      `POVO alert! y'all ain't getting y'all dirty ass hands on this ${item.name}!`
+    );
+  }
 }
 
 bot.login(token);
