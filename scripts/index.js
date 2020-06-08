0c9b718efd77b496d568e1253fe150f246e6c264
s3717566 <s3717566@student.rmit.edu.au>
2020-06-08 16:01:25 +1000

    seperated files, jsonfolder pathing broken

---
+++ scripts/index.js
@@ -0,0 +1,326 @@
+// discord imports
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+// regular imports
+const humanizeDuration = require("humanize-duration");
+const ping = require("minecraft-server-util");
+
+//js files
+const adventureController = require("./adventure.js");
+const partyController = require("./party.js");
+const shopController = require("./shop.js");
+const utils = require("./isekaiUtils.js");
+
+// config
+const configFile = require(utils.jsonFolder + "/config.json");
+
+// config files
+const token = configFile.token;
+const prefix = configFile.prefix;
+
+// constant variables
+const xpToNextLevelMultiplier = 1.5;
+
+bot.on("ready", () => {
+    console.log("Bot is online.");
+});
+
+bot.on("message", (message) => {
+    noPrefixListener(message);
+    if (!message.content.startsWith(prefix)) return;
+
+    let args = message.content.slice(prefix.length).split(" ");
+    if (!checkSaveExists(message) && args[0].toLowerCase() != "create") {
+        message.channel.send(`Sorry you don't have save data ${utils.mentionUser(message.author.id)}. Try doing ${prefix}create to make a new character.`);
+        return;
+    }
+    switch (args[0].toLowerCase()) {
+        case "create":
+            start(message);
+            break;
+
+        case "up":
+            utils.up(message);
+            break;
+
+        case "display":
+            utils.display(message);
+            break;
+
+        case "train":
+            train(message);
+            break;
+
+        case "work":
+            work(message);
+            break;
+
+        case "ping":
+            message.channel.send("don't ping me you fuck");
+            break;
+
+        case "help":
+            message.channel.send("there ain't shit here");
+            break;
+
+        case "ratemywaifu":
+            switch (args[1].toLowerCase()) {
+                case "endorsi":
+                    message.channel.send("widePeepoHappy");
+                    break;
+                default:
+                    message.channel.send("garbo");
+            }
+            break;
+
+        case "prefix":
+            message.channel.send("current prefix is: " + prefix);
+            break;
+
+        case "adventure":
+            adventureController.adventureSwitch(message, args);
+            break;
+
+        case "party":
+            partyController.partySwitch(message, args);
+            break;
+
+        case "shop":
+            shopController.shopSwitch(message, args);
+            break;
+
+        case "buy":
+            shopController.shopBuy(message, args[1]);
+            break;
+
+        case "balance":
+            balance(message);
+            break;
+
+        case "inventory":
+        case "i":
+            inventory(message);
+            break;
+
+        case "e2e":
+            mc(message);
+    }
+});
+
+function checkSaveExists(message) {
+    return utils.getJsonData(utils.saveDataPath).hasOwnProperty(message.author.id);
+}
+
+/*
+Helper function to return an object (player) with all necessary starting stats.
+*/
+function createNewPlayer(authorId) {
+    let currentSaveData = utils.getJsonData(utils.saveDataPath);
+    currentSaveData[authorId] = {
+        level: 0,
+        currentXP: 0,
+        xpToNextLevel: 100,
+        stats: {
+            atk: 10,
+            def: 10,
+            hp: 10,
+        },
+        currentClass: null,
+        classes: [{}],
+        money: 0,
+        partyId: null,
+        inventory: [],
+    };
+    return currentSaveData;
+}
+
+/*
+Helper function to populate all necessary cooldowns for new players.
+*/
+function createCooldowns(authorId) {
+    let currentCooldowns = utils.getJsonData(utils.cooldownsPath);
+    currentCooldowns[authorId] = {
+        train: Date.now(),
+        work: Date.now(),
+        adventure: Date.now(),
+    };
+    return currentCooldowns;
+}
+
+/*
+Creates new save data for a user with their id as the key.
+If id already exists in the file, return immediately.
+*/
+function start(message) {
+    const authorId = message.author.id;
+
+    // If the id already exists in the save file, don't bother with creating a new user.
+    if (utils.getJsonData(utils.saveDataPath).hasOwnProperty(authorId)) {
+        message.channel.send(`Sorry ${utils.mentionUser(authorId)}, you've already been isekai'd. Try prestiging to isekai yourself again!`);
+        return;
+    }
+
+    // Passed all checks, append a new user id key to the current file.
+    utils.writeJson(utils.saveDataPath, createNewPlayer(authorId));
+    // Also add all cooldowns
+    utils.writeJson(utils.cooldownsPath, createCooldowns(authorId));
+
+    message.channel.send(`Created a new save for ${utils.mentionUser(authorId)}!`);
+}
+
+function checkLevelUpAndChangeXP(message, currentSaveData, xpGain) {
+    const authorId = message.author.id;
+    let currentXP = parseInt(currentSaveData[authorId]["currentXP"]);
+    let xpToNextLevel = parseInt(currentSaveData[authorId]["xpToNextLevel"]);
+    let currentLevel = parseInt(currentSaveData[authorId]["level"]);
+
+    // This case means you need to level up the player.
+    if (currentXP + xpGain >= xpToNextLevel) {
+        let leftoverXP = currentXP + xpGain - xpToNextLevel;
+        currentSaveData[authorId]["level"] = currentLevel + 1;
+        currentSaveData[authorId]["currentXP"] = leftoverXP;
+        currentSaveData[authorId]["xpToNextLevel"] = xpToNextLevel * xpToNextLevelMultiplier;
+
+        // This case means just change the currentXP value.
+    } else {
+        currentSaveData[authorId]["currentXP"] = currentXP + xpGain;
+    }
+    // Save the changes
+    utils.writeJson(utils.saveDataPath, currentSaveData);
+
+    // There are scenarios where you can level up twice in one xpGain, hence need to check and recall this function.
+    // (I call utils.getJsonData() here because I think currentSaveData will be the OLD copy of the json, before xp and levels are applied)
+    if (parseInt(utils.getJsonData(utils.saveDataPath)[authorId]["currentXP"]) >= parseInt(utils.getJsonData(utils.saveDataPath)[authorId]["xpToNextLevel"])) {
+        checkLevelUpAndChangeXP(message, utils.getJsonData(utils.saveDataPath), 0);
+    }
+}
+
+/*
+Train command gives player free exp, between 5% - 10% of their xpToNextLevel.
+*/
+function train(message) {
+    const authorId = message.author.id;
+    let trainingCooldown = utils.getJsonData(utils.cooldownsPath)[authorId]["train"];
+
+    if (Date.now() < trainingCooldown) {
+        let remainingTime = humanizeDuration(trainingCooldown - Date.now());
+        message.channel.send(`You've already trained your butt off today ${utils.mentionUser(authorId)}! Train again in ${remainingTime}.`);
+    } else {
+        let currentSaveData = utils.getJsonData(utils.saveDataPath);
+        let cooldowns = utils.getJsonData(utils.cooldownsPath);
+        let previousLevel = currentSaveData[authorId]["level"];
+
+        // The minimum xp gain is 5% of your xp needed to level up.
+        let minXpGain = parseInt(currentSaveData[authorId]["xpToNextLevel"]) * 0.05;
+        // The maximum xp gain is 10% of your xp needed to level up.
+        let maxXpGain = parseInt(currentSaveData[authorId]["xpToNextLevel"]) * 0.1;
+
+        let xpGain = utils.getRandomInt(minXpGain, maxXpGain);
+        message.channel.send(`You've done your training for the day ${utils.mentionUser(authorId)}! You've gained ${xpGain}XP.`);
+        checkLevelUpAndChangeXP(message, currentSaveData, xpGain);
+
+        // If the levels are not equal then the player must've leveled up.
+        if (previousLevel != utils.getJsonData(utils.saveDataPath)[authorId]["level"])
+            message.channel.send(`${utils.mentionUser(authorId)} has leveled up from level ${previousLevel} to level ${utils.getJsonData(utils.saveDataPath)[authorId]["level"]}!`);
+
+        // They can train again in 24 hours.
+        cooldowns[authorId]["train"] = Date.now() + 86400000;
+        utils.writeJson(utils.cooldownsPath, cooldowns);
+    }
+}
+
+/*
+Work command will give money, calculated based on a formula with your level.
+*/
+function work(message) {
+    const authorId = message.author.id;
+    let workCooldown = utils.getJsonData(utils.cooldownsPath)[authorId]["work"];
+
+    if (Date.now() < workCooldown) {
+        let remainingTime = humanizeDuration(workCooldown - Date.now());
+        message.channel.send(`No one is looking for work right now ${utils.mentionUser(authorId)}. Come check back in ${remainingTime} for some work!`);
+    } else {
+        let currentSaveData = utils.getJsonData(utils.saveDataPath);
+        let cooldowns = utils.getJsonData(utils.cooldownsPath);
+
+        let minMoneyGain = 10;
+        let maxMoneyGain = 50;
+        let moneyGain = utils.getRandomInt(minMoneyGain, maxMoneyGain);
+        currentSaveData[authorId]['money'] = moneyGain;
+        utils.writeJson(utils.saveDataPath, currentSaveData);
+
+        // 30 minutes between working
+        cooldowns[authorId]['work'] = Date.now() + 1800000;
+        utils.writeJson(utils.cooldownsPath, cooldowns);
+
+        message.channel.send(`You did some work for the townspeople ${utils.mentionUser(authorId)}! You've earned $${moneyGain}.`);
+    }
+}
+
+function balance(message) {
+    let players = utils.getJsonData(utils.saveDataPath);
+    let player;
+    let playerName = utils.mentionUser(message.author.id);
+
+    for (var id in players) {
+        if (id == message.author.id) {
+            player = players[id];
+        }
+    }
+
+    message.channel.send(`${playerName}'s Wallet: $${player.money}`);
+}
+
+function inventory(message) {
+    let players = utils.getJsonData(utils.saveDataPath);
+    let player;
+    let playerName = utils.mentionUser(message.author.id);
+    let inventoryList = "";
+
+    for (var id in players) {
+        if (id == message.author.id) {
+            player = players[id];
+        }
+    }
+    message.channel.send(`${playerName}'s Inventory:`);
+
+    //Log out the number of the items with the same name instead of just having copies (future ticket)
+    for (let item of player.inventory) {
+        inventoryList += `${item.name} x${item.quantity}\n`;
+    }
+
+    if (inventoryList == "") {
+        message.channel.send(`Sorry you don't have anything.`);
+    } else {
+        message.channel.send(`${inventoryList}`);
+    }
+}
+
+function noPrefixListener(message) {
+    switch (message.content.toLowerCase()) {
+        case "garbo":
+        case "who":
+        case "???":
+            message.channel.send("WH <:OMEGALUL:719390337323237410>");
+    }
+}
+
+function mc(message) {
+    ping("122.111.176.250", 25565, (error, response) => {
+        if (error) {
+            message.channel.send("Server ping timed out.");
+            throw error;
+        }
+        const Embed = new Discord.MessageEmbed()
+            .setTitle('Server Status')
+            .addField('Server IP', response.host)
+            .addField('Server Version', response.version)
+            .addField('Online Players', response.onlinePlayers)
+            .addField('Max Players', response.maxPlayers);
+
+        message.channel.send(Embed);
+    })
+}
+
+bot.login(token);
---
+++ scripts/party.js
@@ -0,0 +1,216 @@
+const utils = require("./isekaiUtils.js");
+const Discord = require("discord.js");
+
+function createNewParty(id, playerId) {
+    let currentSaveData = utils.getJsonData(utils.playerPartiesPath);
+
+    currentSaveData[id] = {
+        leader: playerId,
+        adventureId: 0,
+        members: [playerId],
+    };
+    return currentSaveData;
+}
+
+function partySwitch(message, args) {
+    switch (args[1]) {
+        case "create":
+            partyCreate(message, args[2]);
+            break;
+
+        case "view":
+            partyView(message);
+            break;
+
+        case "leave":
+            partyLeave(message);
+            break;
+
+        case "join":
+            partyJoin(message, utils.getUserFromMention(args[2]));
+            break;
+
+        case "invite":
+            partyInvite(message, utils.getUserFromMention(args[2]));
+            break;
+    }
+}
+
+function partyCreate(message, args) {
+    const authorId = message.author.id;
+    let playerSave = utils.getJsonData(utils.saveDataPath);
+    let playerSavePartyId = playerSave[authorId]['partyId'];
+
+    if (utils.isNull(playerSavePartyId)) {
+        //TODO: check the newPartyId does not already exist.
+        let newPartyId = utils.getRandomInt(0, 10000);
+        utils.writeJson(utils.playerPartiesPath, createNewParty(newPartyId, authorId));
+
+        playerSave[message.author.id]['partyId'] = newPartyId;
+        utils.writeJson(utils.saveDataPath, playerSave);
+
+        message.channel.send(`New party has been created with ID: ${newPartyId}.`);
+    } else {
+        message.channel.send(`You're already in a party with ID: ${playerSavePartyId}`);
+    }
+}
+
+function partyView(message) {
+    const authorId = message.author.id;
+    let playerParty = utils.getJsonData(utils.saveDataPath);
+    let playerPartyId = playerParty[message.author.id]['partyId'];
+
+    if (utils.isNull(playerPartyId)) {
+        message.channel.send(`You're not currently in a party, ${utils.mentionUser(authorId)}.`);
+        return;
+    }
+
+    let party = utils.getJsonData(utils.playerPartiesPath);
+    let members = party[playerPartyId]['members'];
+
+    var printStr = "Party leader is: " + utils.mentionUser(party[playerPartyId]['leader']) + ". The current members of your party are: ";
+    for (var i = 0; i < members.length; i++) {
+        printStr += utils.mentionUser(members[i]) + ", ";
+    }
+    message.channel.send(printStr.substring(0, printStr.length - 1));
+}
+
+function partyLeave(message) {
+    let saveData = utils.getJsonData(utils.saveDataPath);
+    let playerPartyId = saveData[message.author.id]['partyId'];
+
+    if (utils.isNull(playerPartyId)) {
+        message.channel.send(`You're not currently in a party, ${utils.mentionUser(message.author.id)}.`);
+        return;
+    }
+
+    let party = utils.getJsonData(utils.playerPartiesPath);
+    let members = party[playerPartyId]['members'];
+
+    //remove from members list
+    for (var i = 0; i < members.length; i++) {
+        if (members[i] == message.author.id) {
+            members.splice(i, 1);
+        }
+    }
+
+    //check if they were a leader
+    if (party[playerPartyId]['leader'] == message.author.id) {
+        //if no other members, delete party
+        if (members.length == 0) {
+            delete party[playerPartyId];
+            message.channel.send("Party is now empty, deleting.");
+        } else {
+            //else, designate the next person as leader
+            party[playerPartyId]['leader'] = members[0];
+            message.channel.send(`${utils.mentionUser(members[0])} is the new party leader.`);
+        }
+    } else {
+        message.channel.send("You have left the party.");
+    }
+
+    //reset player party id in savedata
+    saveData[message.author.id]["partyId"] = 0;
+
+    utils.writeJson(utils.saveDataPath, saveData);
+    utils.writeJson(utils.playerPartiesPath, party);
+}
+
+function partyJoin(message, targetPlayer) {
+    //grab the party id of the target player
+    //see if the party is full
+    //make it the new message author party id
+    //add the member to the party[members]
+
+    if (utils.isNull(targetPlayer)) {
+        message.channel.send("Please enter the target player.");
+        return;
+    }
+
+    let playerSave = utils.getJsonData(utils.saveDataPath);
+
+    let playerSavePartyId = playerSave[message.author.id]['partyId'];
+
+    if (!utils.isNull(playerSavePartyId)) {
+        message.channel.send(`You're already in a party, ${utils.mentionUser(message.author.id)}. Please leave your current one first.`);
+        return;
+    }
+
+    let targetSavePartyId = playerSave[targetPlayer]["partyId"];
+
+    if (utils.isNull(targetSavePartyId)) {
+        message.channel.send(`Target player ${utils.mentionUser(targetPlayer)} is not currently in a party.`);
+        return;
+    }
+
+    let party = utils.getJsonData(utils.playerPartiesPath);
+    party[targetSavePartyId]['members'].push(message.author.id);
+
+    playerSave[message.author.id]['partyId'] = targetSavePartyId;
+
+    utils.writeJson(utils.saveDataPath, playerSave);
+    utils.writeJson(utils.playerPartiesPath, party);
+
+    message.channel.send("Joined party.");
+}
+
+/*
+Check target player exists, message author is party leader, and party is not full.
+On successful checks, push the target player into the party.
+*/
+function partyInvite(message, targetPlayer) {
+    // Check if targetPlayer is valid
+    if (utils.isNull(targetPlayer)) {
+        message.channel.send(`'${targetPlayer}' is not a valid player. Please enter an existing player.`);
+        return;
+    }
+
+    const authorId = message.author.id;
+    let currentSaveData = utils.getJsonData(utils.saveDataPath);
+    let invitersPartyId = currentSaveData[authorId]['partyId'];
+
+    // Check if inviter is actually in a party
+    if (utils.isNull(invitersPartyId)) {
+        message.channel.send(`${utils.mentionUser(authorId)} is not currently in a party. Join one first before using ${prefix}invite`);
+        return;
+    }
+
+    // Check if targetPlayer is already in a party
+    let inviteePartyId = currentSaveData[targetPlayer]['partyId'];
+    if (!utils.isNull(inviteePartyId)) {
+        message.channel.send(`${utils.mentionUser(targetPlayer)} is already in a party.`);
+        return;
+    }
+
+    message.channel.send(`${utils.mentionUser(authorId)} has invited ${utils.mentionUser(targetPlayer)} to their party.`);
+
+    // Passed all checks, now wait for response from targetPlayer to accept invite.
+    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === targetPlayer, {
+        time: 10000
+    });
+    collector.on('collect', replyMessage => {
+        if (replyMessage.content.toLowerCase() == 'accept') {
+            replyMessage.channel.send(`${utils.mentionUser(targetPlayer)} has accepted ${utils.mentionUser(authorId)}'s invite.`);
+
+            let party = utils.getJsonData(utils.playerPartiesPath);
+            party[invitersPartyId]['members'].push(targetPlayer);
+            currentSaveData[targetPlayer]['partyId'] = invitersPartyId;
+
+            utils.writeJson(utils.saveDataPath, currentSaveData);
+            utils.writeJson(utils.playerPartiesPath, party);
+            collector.stop('accepted');
+        } else if (replyMessage.content.toLowerCase() == 'decline') {
+            replyMessage.channel.send(`${utils.mentionUser(targetPlayer)} has declined ${utils.mentionUser(authorId)}'s invite.`);
+            collector.stop('declined');
+        } else {
+            replyMessage.channel.send(`Please type 'accept' or 'decline', ${utils.mentionUser(targetPlayer)}.`);
+        }
+    });
+    // Can use either collected.has('accepted')/collected.has('declined'), or reason === 'accepted'/reason === 'declined'
+    collector.on('end', (collected, reason) => {
+        if (reason !== 'accepted' && reason !== 'declined')
+            message.channel.send(`${utils.mentionUser(targetPlayer)} did not respond to ${utils.mentionUser(authorId)}'s invite.`);
+    })
+}
+
+module.exports = {partySwitch};
