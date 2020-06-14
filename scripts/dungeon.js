20632ea80a0521e0ab479671790a4904c43ab71b
Duc <duc.h@hotmail.com>
2020-06-14 22:03:10 +1000

    testing

--- scripts/dungeon.js
+++ scripts/dungeon.js
@@ -52,20 +52,17 @@ const dungeonStart = async (message) => {
             // .catch(console.error);
             const newChannel = await asyncExample(message, name);
 
-            console.log("newChannel", newChannel);
+            // console.log("newChannel", newChannel);
             newChannelID = newChannel.id;
             console.log("newChannelID", newChannel.id);
 
-
+            partyConfirmation(message, partyId, newChannel);
         } else {
             message.channel.send(`Only the party leader ${utils.mentionUser(allParties[partyId]["leader"])} can start a dungeon.`);
         }
     } else {
         message.channel.send("You have to be in a party to participate in a dungeon.")
     }
-
-    // Get confirmation from all party members that they're ready.
-    partyConfirmation(message, partyId, newChannel);
 }
 
 //Deletes a dungeon by id or name
@@ -86,6 +83,47 @@ const dungeonDelete = (message) => {
             }
         }
     }
+}
+
+/*
+Helper function to check all party members are 'ready' before starting the actual dungeon run.
+*/
+function partyConfirmation(message, partyId, newChannel) {
+    const allParties = utils.getJsonData(utils.playerPartiesPath);
+    const partyMembers = allParties[partyId]['members']
+
+    let membersReady = {};
+    for (var memberId of partyMembers) {
+        membersReady[memberId] = {ready: false};
+    }
+    message.client.channels.cache.get(newChannelID).send(`Welcome to the dungeon! You have 10 seconds (debug) to type ..ready to confirm you're ready for the dungeon.`);
+
+    // Accept messages as long as they're in the channel (only the party have access to this channel).
+    const collector = new Discord.MessageCollector(newChannel, m => m.channel.id == newChannel.id, {
+        time: 10000
+    });
+    // Players can ready up and unready. When all players have readied up, immediately end the collector.
+    collector.on('collect', replyMessage => {
+        if (replyMessage.content.toLowerCase() == '..ready') {
+            newChannel.send(`${utils.mentionUser(replyMessage.author.id)} is ready.`);
+            membersReady[replyMessage.author.id]['ready'] = true;
+        } else if (replyMessage.content.toLowerCase() == '..unready') {
+            newChannel.send(`${utils.mentionUser(replyMessage.author.id)} is not ready.`);
+            membersReady[replyMessage.author.id]['ready'] = false;
+        }
+
+        // Checks for all keys in memberTurns, if they are all true (if all true, if statement is true)
+        if (Object.keys(membersReady).every(function(key) { return membersReady[key]['ready']})) {
+            newChannel.send(`All party members have readied up! Starting the dungeon in 10 seconds...`);
+            collector.stop('All Ready');
+        }
+    });
+    // Collector can stop when everyone is ready, or by timeout. If reason is not 'All Ready' then close the dungeon.
+    collector.on('end', (collected, reason) => {
+        if (!reason == 'All Ready') {
+            message.channel.send(`The party did not ready up in time. Closing the dungeon...`);
+        }
+    });
 }
 
 const asyncExample = async (message, name) => {
